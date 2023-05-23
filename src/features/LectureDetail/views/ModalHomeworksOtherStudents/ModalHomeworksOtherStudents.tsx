170d01f24722d7778020f80844fb2d7d60131852
Vasilisa Shelkova <100698142+waldblume@users.noreply.github.com>
2023-05-23 12:19:43 +0300

    Closing icon added

--- src/features/LectureDetail/views/ModalHomeworksOtherStudents/ModalHomeworksOtherStudents.tsx
+++ src/features/LectureDetail/views/ModalHomeworksOtherStudents/ModalHomeworksOtherStudents.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useModal } from "react-modal-hook";
 import { Button, Dialog, DialogContent, Stack } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
 import { IModalHomeworksOtherStudents } from "./ModalHomeworksOtherStudents.types";
 import Comments from "../../containers/Comments";
 import CommentsPagination from "../CommentsPagination/CommentsPagination";
@@ -9,6 +10,13 @@ import CommentsTotalElements from "../CommentTotalElements";
 
 const style = {
   scrollContainer: { overflowY: "auto", maxHeight: "calc(100vh - 200px)" },
+  clearIcon: {
+    position: "absolute",
+    top: "20px",
+    right: "23px",
+    cursor: "pointer",
+    zIndex: "1"
+  },
 };
 
 const ModalHomeworksOtherStudents: React.FC<IModalHomeworksOtherStudents> = ({
@@ -16,8 +24,9 @@ const ModalHomeworksOtherStudents: React.FC<IModalHomeworksOtherStudents> = ({
   dataUserId,
 }) => {
   const [showModal, hideModal] = useModal(({ in: open }) => (
-    <Dialog open={open} onClose={hideModal} maxWidth="xl" fullWidth>
+    <Dialog open={open} maxWidth="xl" fullWidth>
       <DialogContent sx={style.scrollContainer} id="scroll-container">
+        <ClearIcon sx={style.clearIcon} onClick={hideModal} />
         <HomeworkItem dataHomeWorkByLecture={item} dataUserId={dataUserId} />
         <Comments id={item.id!}>
           <CommentsPagination />
