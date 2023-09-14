5c12e941722b736044d2c3bc447fee612c19ecdc
Vasilisa Shelkova <100698142+waldblume@users.noreply.github.com>
2023-09-14 16:22:08 +0200

    Conflicts resolved

---
+++ src/shared/components/layout/styles.ts
@@ -0,0 +1,11 @@
+export const style = {
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  },
+  content: {
+    flexGrow: 1,
+    minWidth: "100%",
+  },
+};
