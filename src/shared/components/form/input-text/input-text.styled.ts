8073298c4a3a6e9ecf99453d245ee8b4dc2a9845
Vasilisa Shelkova <100698142+waldblume@users.noreply.github.com>
2023-11-06 17:31:10 +0100

    Validation style fixed

--- src/shared/components/form/input-text/input-text.styled.ts
+++ src/shared/components/form/input-text/input-text.styled.ts
@@ -1,10 +1,6 @@
 import { styled } from "@mui/system";
 import { FormHelperText } from "@mui/material";
 
-export const StyledImgBox = styled("li")({
-  "& > img": { marginRight: "20px", flexShrink: 0 },
-});
-
 export const StyledFormHelperText = styled(FormHelperText)({
   position: "absolute",
   top: "52px",
