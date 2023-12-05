7ebcffa03a36fe705d90f7713430da8cd57bb809
nikita <nikitaelin455@gmail.com>
2023-12-05 18:37:57 +0300

    QAGDEV-410 - Create dynamic calendar v 2.0

--- src/features/training-purchases/views/training-purchases.tsx
+++ src/features/training-purchases/views/training-purchases.tsx
@@ -1,7 +1,14 @@
-import { FC, useState } from "react";
-import { Box, Container, Grid, Typography } from "@mui/material";
-import { DateCalendar, LocalizationProvider } from "@mui/x-date-pickers";
+import { FC, useEffect, useRef, useState } from "react";
+import { Badge, Box, Container, Grid, Typography } from "@mui/material";
+import {
+  DateCalendar,
+  LocalizationProvider,
+  PickersDay,
+  type PickersDayProps,
+} from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
+import { DayCalendarSkeleton } from "@mui/x-date-pickers/DayCalendarSkeleton";
 import { ITrainings } from "./training-purchases.types";
 import {
   StyledBox,
@@ -13,9 +20,97 @@ import {
 } from "./training-purchases.styled";
 import { white } from "../../../theme/colors";
 
+function getRandomNumber(min: number, max: number) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function fakeFetch(date: Dayjs, { signal }: { signal: AbortSignal }) {
+  return new Promise<{ daysToHighlight: number[] }>((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      const daysInMonth = date.daysInMonth();
+      const daysToHighlight = [1, 2, 3].map(() =>
+        getRandomNumber(1, daysInMonth)
+      );
+
+      resolve({ daysToHighlight });
+    }, 500);
+
+    signal.onabort = () => {
+      clearTimeout(timeout);
+      reject(new DOMException("aborted", "AbortError"));
+    };
+  });
+}
+
+const initialValue = dayjs("2022-04-17");
+
+function ServerDay(
+  props: PickersDayProps<Dayjs> & { highlightedDays?: number[] }
+) {
+  const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
+
+  const isSelected =
+    !props.outsideCurrentMonth &&
+    highlightedDays.indexOf(props.day.date()) >= 0;
+
+  return (
+    <Badge
+      key={props.day.toString()}
+      overlap="circular"
+      badgeContent={isSelected ? "🌚" : undefined}
+    >
+      <PickersDay
+        {...other}
+        outsideCurrentMonth={outsideCurrentMonth}
+        day={day}
+      />
+    </Badge>
+  );
+}
+
 const TrainingPurchases: FC<ITrainings> = ({ data }) => {
   const { trainingPurchases } = data;
-  const [showCalendar, setShowCalendar] = useState(false);
+
+  const requestAbortController = useRef<AbortController | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [highlightedDays, setHighlightedDays] = useState([1, 2, 15]);
+
+  const fetchHighlightedDays = (date: Dayjs) => {
+    const controller = new AbortController();
+    fakeFetch(date, {
+      signal: controller.signal,
+    })
+      .then(({ daysToHighlight }) => {
+        setHighlightedDays(daysToHighlight);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        // ignore the error if it's caused by `controller.abort`
+        if (error.name !== "AbortError") {
+          throw error;
+        }
+      });
+
+    requestAbortController.current = controller;
+  };
+
+  useEffect(() => {
+    fetchHighlightedDays(initialValue);
+    // abort request on unmount
+    return () => requestAbortController.current?.abort();
+  }, []);
+
+  const handleMonthChange = (date: Dayjs) => {
+    if (requestAbortController.current) {
+      // make sure that you are aborting useless requests
+      // because it is possible to switch between months pretty quickly
+      requestAbortController.current.abort();
+    }
+
+    setIsLoading(true);
+    setHighlightedDays([]);
+    fetchHighlightedDays(date);
+  };
 
   return (
     <Container>
@@ -23,6 +118,7 @@ const TrainingPurchases: FC<ITrainings> = ({ data }) => {
       <StyledGrid container spacing={2} sx={{ position: "relative" }}>
         {trainingPurchases?.map((item, index) => {
           const { id, name } = item?.trainingTariff.training || {};
+          const [showCalendar, setShowCalendar] = useState(false);
 
           return (
             <Grid
@@ -52,7 +148,20 @@ const TrainingPurchases: FC<ITrainings> = ({ data }) => {
                   sx={{ position: "absolute", backgroundColor: white }}
                 >
                   <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DateCalendar />
+                    <DateCalendar
+                      defaultValue={initialValue}
+                      loading={isLoading}
+                      onMonthChange={handleMonthChange}
+                      renderLoading={() => <DayCalendarSkeleton />}
+                      slots={{
+                        day: ServerDay,
+                      }}
+                      slotProps={{
+                        day: {
+                          highlightedDays,
+                        } as any,
+                      }}
+                    />
                   </LocalizationProvider>
                 </Box>
               )}
