01f05111c862acc67776b1c5e2d2b195581bb376
nikita <nikitaelin455@gmail.com>
2023-12-10 19:21:01 +0300

    Hook - use-role-access v 2.0

--- src/widgets/header/header.test.tsx
+++ src/widgets/header/header.test.tsx
@@ -1,6 +1,5 @@
 import { MemoryRouter } from "react-router-dom";
 import { render } from "test/utils-test";
-import { UserRole } from "api/graphql/generated/graphql";
 
 import Header from "./header";
 
@@ -8,23 +7,7 @@ describe("Header", () => {
   it("userRole is Student", () => {
     const { asFragment } = render(
       <MemoryRouter>
-        <Header userRoles={[UserRole.Student]} />
-      </MemoryRouter>
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
-  it("userRole is MentorsContainer", () => {
-    const { asFragment } = render(
-      <MemoryRouter>
-        <Header userRoles={[UserRole.Mentor]} />
-      </MemoryRouter>
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
-  it("userRole is Master", () => {
-    const { asFragment } = render(
-      <MemoryRouter>
-        <Header userRoles={[UserRole.Master]} />
+        <Header />
       </MemoryRouter>
     );
     expect(asFragment()).toMatchSnapshot();
