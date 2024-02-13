b075e3b2dbd3886c5bcf11e8976d5fb20f74b09a
Vasilisa Shelkova <100698142+shelkovichka@users.noreply.github.com>
2024-02-13 17:01:15 +0300

    Hover added to link in user row

--- src/shared/components/user-row/user-name/user-name.styled.ts
+++ src/shared/components/user-row/user-name/user-name.styled.ts
@@ -1,7 +1,10 @@
 import { styled } from "@mui/system";
 import { Link } from "@mui/material";
 
-export const StyledLink = styled(Link)({
+export const StyledLink = styled(Link)(({ theme }) => ({
   textDecoration: "none",
   textAlign: "start",
-}) as typeof Link;
+  "&:hover": {
+    textDecoration: "underline",
+  },
+})) as typeof Link;
