393ae730803fb3936e0766fd8063f51a113eed01
Vasilisa Shelkova <100698142+shelkovichka@users.noreply.github.com>
2024-02-13 16:45:34 +0300

    Modal added to delete comment

--- src/features/lecture-detail/views/delete-comment/delete-comment.styled.ts
+++ src/features/lecture-detail/views/delete-comment/delete-comment.styled.ts
@@ -1,6 +1,6 @@
 import { styled } from "@mui/system";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { IconButton } from "@mui/material";
+import { Box, Button, DialogContent, IconButton, Stack } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
 
 export const StyledIconButton = styled(IconButton)(({ theme }) => ({
@@ -15,4 +15,31 @@ export const StyledSpinner = styled(CircularProgress)(({ theme }) => ({
 export const StyledDeleteIcon = styled(DeleteIcon)(({ theme }) => ({
   color: theme.palette.app.primary,
   width: "16px",
+}));
+
+export const StyledWrapper = styled(Box)({
+  padding: "30px 20px 10px",
+  width: "290px",
+});
+
+export const StyledDialogContent = styled(DialogContent)({
+  textAlign: "center",
+});
+
+export const StyledStack = styled(Stack)(({ theme }) => ({
+  width: "100%",
+  gap: "30px",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  marginBottom: "15px",
+}));
+
+export const StyledButton = styled(Button)(({ theme }) => ({
+  width: "145px",
+  color: theme.palette.app.white,
+}));
+
+export const StyledCancelButton = styled(Button)(({ theme }) => ({
+  width: "145px",
+  color: theme.palette.app.black,
 }));
