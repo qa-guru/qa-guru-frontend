ef4478e180e1b6ef53ebc53753d20ca132562432
Maksim Fedotov <f42.dobro@gmail.com>
2024-02-19 16:12:35 +0700

    comments

--- src/pages/auth/index.ts
+++ src/pages/auth/index.ts
@@ -3,3 +3,9 @@ export { default as SignUpPage } from "./sign-up";
 export { default as ResetPage } from "./reset";
 export { default as SetPasswordPage } from "./set-password";
 export { default as ConfirmTokenPage } from "./confirm-token";
+// причина по которой export default плохо
+
+
+// а еще, вся эта папка pages - бойлерплейтный однотипный код. не нужно
+// нужно то что у вас называется container (в основном это все таки page)
+// класть прям сюда и импортировать хуки и view из features
