4528b25e722e87f929e871c18a7a7413558f4008
Vasilisa Shelkova <100698142+shelkovichka@users.noreply.github.com>
2024-02-29 10:40:19 +0100

    QAGDEV-487 – Remove animation in kanban

--- src/features/kanban/constants/constants.ts
+++ src/features/kanban/constants/constants.ts
@@ -18,10 +18,6 @@ export const STANDARD_QUERY_DEFAULTS = {
 export const UI_CONSTANTS = {
   MIN_COLUMN_WIDTH: "65%",
   MAX_COLUMN_WIDTH: "100%",
-  MIN_DETAILS_WITH: "0",
-  MAX_DETAILS_WITH: "34%",
-  ANIMATE_DURATION: 0.4,
-  ANIMATE_MARGIN: 2,
 };
 
 export const STEP = 1;
