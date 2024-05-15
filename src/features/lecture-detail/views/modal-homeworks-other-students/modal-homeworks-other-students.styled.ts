e7cd23dd9efc0413f4fc551501fc995d06f171e7
Vasilisa Shelkova <100698142+shelkovichka@users.noreply.github.com>
2024-05-15 14:33:41 +0200

    QAGDEV-577 – Fix style in modals

--- src/features/lecture-detail/views/modal-homeworks-other-students/modal-homeworks-other-students.styled.ts
+++ src/features/lecture-detail/views/modal-homeworks-other-students/modal-homeworks-other-students.styled.ts
@@ -26,7 +26,7 @@ export const StyledDialogContent = styled(DialogContent)(({ theme }) => ({
   overflowY: "auto",
   scrollbarWidth: "none",
   height: "calc(100dvh - 50px)",
-  padding: "10px",
+  padding: "10px 10px 80px",
   [theme.breakpoints.up("sm")]: {
     height: "calc(100dvh - 50px)",
     padding: "30px 15px 15px",
