4ca4662f7a9b89ccb444a041de3cbc72b2ca552a
nikita <nikitaelin455@gmail.com>
2024-07-08 18:14:28 +0300

    QAGDEV-652 - Fix bug with url link in TextView

--- src/shared/components/text-editor/text-view/text-view.tsx
+++ src/shared/components/text-editor/text-view/text-view.tsx
@@ -1,4 +1,4 @@
-import { FC, useMemo } from "react";
+import { FC } from "react";
 import { RichTextReadOnly } from "shared/lib/mui-tiptap";
 import { Maybe } from "api/graphql/generated/graphql";
 import { FormControl } from "@mui/material";
@@ -10,42 +10,12 @@ interface TextViewProps {
   content?: Maybe<any>;
 }
 
-const convertTextToLinks = (content?: Maybe<string>) => {
-  const regex = /(?<!<[^<>]*)\b(?:https?|www)\S*(?![^<>]*>)/g;
-  let modifiedContent = content;
-
-  if (content) {
-    const matches = [...content.matchAll(regex)];
-    let lastIndex = 0;
-    const segments = [];
-
-    matches.forEach((match) => {
-      const matchStart = match.index;
-      const matchEnd = Number(matchStart) + match[0].length;
-
-      segments.push(content.substring(lastIndex, matchStart));
-
-      segments.push(
-        `<a style="word-break: break-all" href="${match[0]}" target="_blank" rel="noopener noreferrer">${match[0]}</a>`
-      );
-
-      lastIndex = matchEnd;
-    });
-
-    segments.push(content.substring(lastIndex));
-    modifiedContent = segments.join("");
-  }
-
-  return modifiedContent;
-};
-
 const TextView: FC<TextViewProps> = ({ content }) => {
   const extensions = useExtensions();
-  const modifiedContent = useMemo(() => convertTextToLinks(content), [content]);
 
   return (
     <FormControl sx={{ wordBreak: "break-word" }}>
-      <RichTextReadOnly content={modifiedContent} extensions={extensions} />
+      <RichTextReadOnly content={content} extensions={extensions} />
     </FormControl>
   );
 };
