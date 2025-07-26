8db1c07a5db1b2ab2b3ff4f7752b8b85ae95b9cd
Nik Elin <67843454+nik1999777@users.noreply.github.com>
2025-07-26 22:51:54 +0300

    QAGDEV-726 - Бесконечная загрузка при открытии урока на тарифе Самостоятельный

--- src/pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-import TestContainer from "features/test/containers/test-container";
-
-const TestPage: FC = () => {
-  return <TestContainer />;
-};
-
-export default TestPage;
