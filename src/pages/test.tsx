869df432639b75011dfdfcb170a21ea38f7e6512
Nik Elin <67843454+nik1999777@users.noreply.github.com>
2025-08-15 13:38:53 +0300

    Merge conflict

---
+++ src/pages/test.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import TestContainer from "features/test/containers/test-container";
+
+const TestPage: FC = () => {
+  return <TestContainer />;
+};
+
+export default TestPage;
